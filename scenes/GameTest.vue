0501f66d896fd7356a391ea6995b88e1f020f75d
kolaflash <kolaflash@GAMBIT>
2024-11-08 00:15:36 +0100

    Native drag/drop is not supported for mobile >.<

--- scenes/GameTest.vue
+++ scenes/GameTest.vue
@@ -61,16 +61,37 @@ const game: Game = {
     },
   },
 };
+
+function focusCard(event: MouseEvent) {
+  event.preventDefault();
+  (event.target as HTMLElement).classList.toggle("focused");
+}
+
+function drag(event: DragEvent) {
+  event.dataTransfer!.setData("card", String(event.target));
+}
+
+function drop(event: DragEvent) {
+  console.log(event.dataTransfer!.getData("card"));
+}
+
+function allowDrop(event: DragEvent) {
+  event.preventDefault();
+}
 </script>
 
 <template>
   <div class="container game-test">
-    <ul
-      class="player-hand"
-      v-for="(card, index) in game.player.hand"
-      :key="index"
-    >
-      <li class="card">
+    <div class="game-test-board" @dragover="allowDrop" @drop="drop"></div>
+    <ul class="player-hand">
+      <li
+        class="card"
+        @click.right="focusCard"
+        @dragstart="drag"
+        draggable="true"
+        v-for="(card, index) in game.player.hand"
+        :key="index"
+      >
         <div class="card-title">{{ card.title }}</div>
       </li>
     </ul>
@@ -79,23 +100,52 @@ const game: Game = {
 
 <style>
 .game-test {
+  flex-flow: column nowrap;
+
+  .game-test-board {
+    background: rgba(0, 0, 0, 0.5);
+    border: 1px solid whitesmoke;
+    border-radius: 8px;
+    flex: 1 1 auto;
+    width: 100%;
+  }
+
   .player-hand {
     display: flex;
     flex-flow: row nowrap;
+    gap: 8px;
     justify-content: space-between;
-    align-items: center;
+    align-items: flex-end;
+    list-style: none;
+    margin: 0;
+    padding: 8px;
+    flex: 0 1 auto;
   }
+
   .card {
     display: flex;
     flex-flow: column nowrap;
     justify-content: space-between;
     align-items: center;
     border: 1px solid whitesmoke;
-    border-radius: 0.5rem;
-    padding: 0.5rem;
-    width: 8rem;
-    height: 12rem;
+    border-radius: 8px;
+    padding: 8px;
+    width: 120px;
+    height: 160px;
     color: whitesmoke;
+    transition: all 144ms ease-out;
+
+    &:hover {
+      transition: all 144ms ease-out;
+      transform: scale(1.1) translateY(-8px);
+    }
+
+    &.focused {
+      transition: all 144ms ease-out;
+      transform: none;
+      width: 180px;
+      height: 240px;
+    }
   }
 }
 </style>
