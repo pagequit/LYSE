cc88e1137daa1888830a2c487f8f3698e729d09a
kolaflash <kolaflash@GAMBIT>
2025-01-17 00:04:45 +0100

    WIP

--- engine/lib/CollisionShape.ts
+++ engine/lib/CollisionShape.ts
@@ -1,10 +1,12 @@
 import { type Vector } from "./Vector.ts";
 import { createRenderable, type Renderable } from "./Renderable.ts";
 
-export type CollisionShape = {
+export type CollisionShape<T> = {
   type: CollisionShapeType;
   position: Vector;
-} & Renderable;
+} & T;
+
+export type Circle = { radius: number };
 
 export enum CollisionShapeType {
   Circle,
@@ -12,27 +14,21 @@ export enum CollisionShapeType {
 }
 
 function renderCircle(
-  this: CollisionShape & { size: number },
+  this: CollisionShape<Circle>,
   ctx: CanvasRenderingContext2D,
 ): void {
   ctx.fillStyle = "rgba(255, 0, 128, 0.5)";
   ctx.beginPath();
-  ctx.arc(this.position.x, this.position.y, this.size, 0, 2 * Math.PI);
+  ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
   ctx.fill();
 }
 
 export function createCollisionShapeCircle(
   position: Vector,
-  size: number,
-): CollisionShape {
+  radius: number,
+): CollisionShape<Circle> & Renderable {
   return createRenderable(
-    { type: CollisionShapeType.Circle, position, size },
+    { type: CollisionShapeType.Circle, position, radius },
     renderCircle,
   );
-}
-
-type CollisionPool = Map<CollisionShape, CollisionShape>;
-
-export function createCollisionPool(): CollisionPool {
-  return new Map();
 }
--- game/entity/Player.ts
+++ game/entity/Player.ts
@@ -8,7 +8,9 @@ import {
 import { getDirection, isZero, type Vector } from "../../engine/lib/Vector.ts";
 import { input } from "../../engine/system/Input.ts";
 import {
+  type Circle,
   type CollisionShape,
+  CollisionShapeType,
   createCollisionShapeCircle,
 } from "../../engine/lib/CollisionShape.ts";
 
@@ -31,7 +33,7 @@ export type Player = {
   velocity: Vector;
   speedMultiplier: number;
   animations: Record<State, Sprite>;
-  collisionShape: CollisionShape;
+  collisionShape: CollisionShape<Circle>;
 };
 
 export function createPlayer(
@@ -132,20 +134,27 @@ export function processPlayer(
   player.velocity.y = input.vector.y * delta * player.speedMultiplier;
 
   for (let i = 0; i < collisionShapes.length; i++) {
-    const dx =
-      player.position.x + player.velocity.x - collisionShapes[i].position.x;
-    const dy =
-      player.position.y + player.velocity.y - collisionShapes[i].position.y;
-    const distance = Math.hypot(dx, dy);
+    switch (collisionShapes[i].type) {
+      case CollisionShapeType.Circle: {
+        const dx =
+          player.position.x + player.velocity.x - collisionShapes[i].position.x;
+        const dy =
+          player.position.y + player.velocity.y - collisionShapes[i].position.y;
+        const distance = Math.hypot(dx, dy);
 
-    // all shapes are circles with a radius of 16 for now
-    if (distance <= collisionShapes[i].size + player.collisionShape.size) {
-      if (Math.abs(dx) > Math.abs(dy)) {
-        player.velocity.x += dx > 0 ? 2 : -2;
-        player.velocity.y += dy > 0 ? 1 : -1;
-      } else {
-        player.velocity.x += dx > 0 ? 1 : -1;
-        player.velocity.y += dy > 0 ? 2 : -2;
+        if (
+          distance <=
+          collisionShapes[i].radius + player.collisionShape.radius
+        ) {
+          if (Math.abs(dx) > Math.abs(dy)) {
+            player.velocity.x += dx > 0 ? 2 : -2;
+            player.velocity.y += dy > 0 ? 1 : -1;
+          } else {
+            player.velocity.x += dx > 0 ? 1 : -1;
+            player.velocity.y += dy > 0 ? 2 : -2;
+          }
+        }
+        break;
       }
     }
   }
