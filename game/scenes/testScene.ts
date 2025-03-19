594a6c071abc1cc951323d5cf14c1eef05804906
kolaflash <kolaflash@GAMBIT>
2025-03-19 13:18:58 +0100

    Test

--- game/scenes/testScene.ts
+++ game/scenes/testScene.ts
@@ -42,7 +42,7 @@ import {
   type KinematicBody,
   setActiveKinematicBodies,
 } from "../../engine/KinematicBody.ts";
-import { isZero } from "../../engine/Vector.ts";
+import { monitor } from "../../engine/monitor.ts";
 
 const scene: Scene = createScene(process, {
   width: 2048,
@@ -132,6 +132,9 @@ const kinematicBodies = [
 
 const activeKinematicBodies: Array<KinematicBody<Circle | Rectangle>> = [];
 
+const playerMonitor = document.createElement("div");
+monitor.appendChild(playerMonitor);
+
 function process(ctx: CanvasRenderingContext2D, delta: number): void {
   grid.render(ctx);
 
@@ -199,6 +202,8 @@ function process(ctx: CanvasRenderingContext2D, delta: number): void {
 
   pointerNode.position = pointer.position;
   paintNode(pointerNode, ctx, "rgba(255, 255, 255, 0.5)");
+
+  playerMonitor.innerText = `player: (${player.velocity.x}, ${player.velocity.y})`;
 }
 
 export default scene;
