6867c24d63c82c7f92d8b3669d332007e5eb2eb5
kolaflash <kolaflash@GAMBIT>
2025-04-14 21:33:20 +0200

    Screw this page-build sh*t

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
 import { defineConfig } from "vite";
-// import { readdir } from "node:fs/promises";
+import { readdir } from "node:fs/promises";
 
 export default defineConfig({
-  base: "/LYSE/",
   server: { port: 3080 },
   build: { target: "esnext" },
-  // plugins: [
-  //   {
-  //     name: "asset-indexer",
-  //     buildStart: async function () {
-  //       const assets = await readdir("public", { withFileTypes: true });
-  //       const index = assets
-  //         .filter((a) => a.isFile() && a.name !== "assetindex.json")
-  //         .map((a) => a.name);
-  //       await Bun.write("public/assetindex.json", JSON.stringify(index));
-  //     },
-  //   },
-  // ],
+  plugins: [
+    {
+      name: "asset-indexer",
+      buildStart: async function () {
+        const assets = await readdir("public", { withFileTypes: true });
+        const index = assets
+          .filter((a) => a.isFile() && a.name !== "assetindex.json")
+          .map((a) => a.name);
+        await Bun.write("public/assetindex.json", JSON.stringify(index));
+      },
+    },
+  ],
 });
